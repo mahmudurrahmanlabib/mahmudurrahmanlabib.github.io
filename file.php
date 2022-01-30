e10ae7980172487eb5954706c4a1519f7ed3bab1
Mahmudur Rahman Labib <labibrahman555@gmail.com>
2022-01-31 05:22:18 +0600

    Issues Resolved!

---
+++ file.php
@@ -0,0 +1,25 @@
+<?php
+
+  $receiving_email_address = 'labibrahman555@gmail.com';
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
